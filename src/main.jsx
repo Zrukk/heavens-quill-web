7f18d93649aec6ab0c9c16e4e600a00a67516811
Zrukk <muhnauwavi6474@gmail.com>
2026-09-14 15:29:00 +0700

    Wrap App component with SettingsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './lib/AuthContext.jsx'
+import { SettingsProvider } from './lib/SettingsContext.jsx'  // ← HARUS ADA
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <SettingsProvider>   {/* ← HARUS ADA */}
+          <App />
+        </SettingsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
