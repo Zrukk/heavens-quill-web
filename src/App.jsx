3e26f9d40ed62c08563f5a370b6a02f7f9e17e46
Zrukk <muhnauwavi6474@gmail.com>
2026-09-15 14:48:07 +0700

    Add Footer and new static pages to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import NovelList from './pages/NovelList'
 import NovelDetail from './pages/NovelDetail'
 import ChapterReader from './pages/ChapterReader'
@@ -8,22 +9,29 @@ import Admin from './pages/Admin'
 import Profile from './pages/Profile'
 import PublicProfile from './pages/PublicProfile'
 import Settings from './pages/Settings'
+import { About, Contact, Privacy } from './pages/StaticPages'
 import BackToTop from './components/BackToTop'
 
 export default function App() {
   return (
-    <div style={{ minHeight: '100vh' }}>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<NovelList />} />
-        <Route path="/novel/:slug" element={<NovelDetail />} />
-        <Route path="/novel/:slug/chapter/:number" element={<ChapterReader />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/profil" element={<Profile />} />
-        <Route path="/pembaca/:userId" element={<PublicProfile />} />
-        <Route path="/pengaturan" element={<Settings />} />
-      </Routes>
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<NovelList />} />
+          <Route path="/novel/:slug" element={<NovelDetail />} />
+          <Route path="/novel/:slug/chapter/:number" element={<ChapterReader />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/profil" element={<Profile />} />
+          <Route path="/pembaca/:userId" element={<PublicProfile />} />
+          <Route path="/pengaturan" element={<Settings />} />
+          <Route path="/tentang" element={<About />} />
+          <Route path="/kontak" element={<Contact />} />
+          <Route path="/kebijakan-privasi" element={<Privacy />} />
+        </Routes>
+      </div>
+      <Footer />
       <BackToTop />
     </div>
   )
