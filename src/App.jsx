fe3d248fcfa60e53fc801bd25ff0099afb4f18a4
Zrukk <muhnauwavi6474@gmail.com>
2026-09-23 12:34:08 +0700

    Add RouteTransition component to routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Leaderboard from './pages/Leaderboard'
 import Titles from './pages/Titles'
 import { About, Contact, Privacy, Rules } from './pages/StaticPages'
 import BackToTop from './components/BackToTop'
+import RouteTransition from './components/RouteTransition'
 import { useScrollToAnchor } from './lib/useScrollToAnchor'
 
 export default function App() {
@@ -22,22 +23,24 @@ export default function App() {
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Navbar />
       <div style={{ flex: 1 }}>
-        <Routes>
-          <Route path="/" element={<NovelList />} />
-          <Route path="/novel/:slug" element={<NovelDetail />} />
-          <Route path="/novel/:slug/chapter/:number" element={<ChapterReader />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/profil" element={<Profile />} />
-          <Route path="/pembaca/:userId" element={<PublicProfile />} />
-          <Route path="/pengaturan" element={<Settings />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/gelar" element={<Titles />} />
-          <Route path="/tentang" element={<About />} />
-          <Route path="/kontak" element={<Contact />} />
-          <Route path="/peraturan" element={<Rules />} />
-          <Route path="/kebijakan-privasi" element={<Privacy />} />
-        </Routes>
+        <RouteTransition>
+          <Routes>
+            <Route path="/" element={<NovelList />} />
+            <Route path="/novel/:slug" element={<NovelDetail />} />
+            <Route path="/novel/:slug/chapter/:number" element={<ChapterReader />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/profil" element={<Profile />} />
+            <Route path="/pembaca/:userId" element={<PublicProfile />} />
+            <Route path="/pengaturan" element={<Settings />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/gelar" element={<Titles />} />
+            <Route path="/tentang" element={<About />} />
+            <Route path="/kontak" element={<Contact />} />
+            <Route path="/peraturan" element={<Rules />} />
+            <Route path="/kebijakan-privasi" element={<Privacy />} />
+          </Routes>
+        </RouteTransition>
       </div>
       <Footer />
       <BackToTop />
