2446c07b9941cc34ce051839ae1fefe9c7410587
Zrukk <muhnauwavi6474@gmail.com>
2026-09-25 10:24:33 +0700

    Wrap App component with StreakProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './lib/AuthContext.jsx'
 import { SettingsProvider } from './lib/SettingsContext.jsx'
+import { StreakProvider } from './lib/StreakContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <SettingsProvider>
-          <App />
+            <StreakProvider>
+              <App />
+            </StreakProvider>
         </SettingsProvider>
       </AuthProvider>
     </BrowserRouter>
